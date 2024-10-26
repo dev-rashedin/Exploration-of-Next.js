647566f7f70d8a3dfd523211b0a73e7150b60641
RU063 <rashedinislam.06@gmail.com>
2024-10-26 10:00:04 +0600

    custom loading added for categories page

--- src/app/categories/[...features]/page.jsx
+++ src/app/categories/[...features]/page.jsx
@@ -1,7 +1,12 @@
+import getAllPosts from "@/lib/getAllPosts";
+import Link from "next/link";
 
 async function categoryDetailsPage({ params }) {
 const {features} = await params;
-console.log(features)
+  console.log(features)
+  
+
+  const posts = await getAllPosts();
 
   if (features.length === 1) {
     return <p>Featured params: {features[0]}</p>
@@ -16,6 +21,18 @@ console.log(features)
   return (
     <div className='min-h-[60vh] flex justify-center items-center'>
       category Details Page
+      <ul className='mt-6 px-6'>
+        {posts.map((post, idx) => (
+          <li key={post.id} className='mb-2 cursor-pointer pb-1'>
+            <Link href={`/posts/${post.id}`}>
+              <span className='border-b px-1 rounded-xl mr-1 border-yellow-600'>
+                {idx + 1}.
+              </span>{' '}
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/app/categories/loading.jsx
@@ -0,0 +1,9 @@
+
+function loading() {
+  return (
+    <div className='min-h-[60vh] flex justify-center items-center'>
+      loading categories......
+    </div>
+  );
+}
+export default loading
